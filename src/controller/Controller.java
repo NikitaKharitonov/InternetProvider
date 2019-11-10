5b03e5b468d841297c4e4fbf7c4d6708b02a7eb1
Anteii <34139858+Anteii@users.noreply.github.com>
2019-11-10 18:02:23 +0400

    test

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,29 @@
 package controller;
 
+import model.Model;
+import view.View;
+
+import java.util.function.Consumer;
+
 public class Controller {
+
+    private Model model;
+    private View view;
+
+    private class Handler implements Consumer{
+        @Override
+        public void accept(Object o) {
+            //Обработка запроса
+            String type = view.getCommandType();
+
+        }
+    }
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+        //Установка обработчика запроса
+        this.view.setHandler(new Handler());
+    }
 
 }

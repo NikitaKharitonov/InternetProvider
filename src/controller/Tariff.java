682f2ad843fa3a1e0620a43e1a131cd70947a1f1
Anteii <34139858+Anteii@users.noreply.github.com>
2019-11-18 19:39:37 +0400

    Minor fix 1) getService() now return clone of service of object 2) Added javadoc

--- src/controller/Tariff.java
+++ src/controller/Tariff.java
@@ -1,25 +1,46 @@
 package controller;
 
 import model.services.Service;
-
+/**
+ * Class Tariff represents tariff for View and controlling if it change
+ * @author anteii
+ * @version 0.1
+ * */
 public class Tariff {
-
+    /**
+     * Service object
+     * */
     private Service service;
+    /**
+     * Service id
+     * */
     private int id;
+    /**
+     * Flag if service was changed
+     * */
     private boolean isEdited;
-
+    /**
+     * Package-Private constructor
+     * @param service
+     *          Service object wich will be aggregated
+     * */
     Tariff(Service service) {
         this.service = service;
         this.isEdited = false;
     }
-
+    /**
+     * Default setter for service field
+     * It also set up flag
+     * */
     public void setService(Service service) {
         this.service = service;
         isEdited = true;
     }
-    // copying ctr
-    public Service getService() {
-        return service;
+    /**
+     *@return clone of service object
+     * */
+    public Service getService() throws CloneNotSupportedException {
+        return (Service) service.clone();
     }
 
     public Integer getId() {

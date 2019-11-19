865cfe1f034bf877bf8f92f9a911c07f69e99ac8
dabutskikh <dabutskikh@gmail.com>
2019-11-19 13:51:07 +0400

    Class ConsoleView is in a separate file

--- src/view/Command.java
+++ src/view/Command.java
@@ -5,15 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum Command {
-    /**/GET_TARIFF("^get (Internet|Television|Phone) id=([0-9]+)$"),
-    /**/GET_USER("^get User id=([0-9]+)$"), //--------
-    /**/GET_TARIFFS("^get (Internets|Televisions|Phones)$"),
-    /**/GET_SERVICES("^get Services$"),
+    GET_TARIFF("^get (Internet|Television|Phone) id=([0-9]+)$"),
+    GET_USER("^get User id=([0-9]+)$"), //--------
+    GET_TARIFFS("^get (Internets|Televisions|Phones)$"),
+    GET_SERVICES("^get Services$"),
 
-    /**/CHANGE_USER("^set User id=([0-9]+)( name=([а-яА-Я ]+))?( phone=(\\+7[0-9]{10}))?( email=([A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}))?$"),
-    /*!*/CHANGE_INTERNET("^set Internet id=([0-9]+)( name=([а-яА-Я ]+))?( speed=([0-9]+))?( antivirus=(true|false))?( connectionType=(ADSL|DialUp|ISDN|Cable|Fiber))?$"),
-    /*!*/CHANGE_TELEVISION("^set Television id=([0-9]+)( name=([а-яА-Я ]+))?( numberOfChannels=([0-9]+))?$"),
-    /*!*/CHANGE_PHONE("^set Phone id=([0-9]+)( name=([а-яА-Я ]+))?( callsMinCount=([0-9]+))?( smsCount=([0-9]+))?$"),
+    CHANGE_USER("^set User id=([0-9]+)( name=([а-яА-Я ]+))?( phone=(\\+7[0-9]{10}))?( email=([A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}))?$"),
+    CHANGE_INTERNET("^set Internet id=([0-9]+)( name=([а-яА-Я ]+))?( speed=([0-9]+))?( antivirus=(true|false))?( connectionType=(ADSL|DialUp|ISDN|Cable|Fiber))?$"),
+    CHANGE_TELEVISION("^set Television id=([0-9]+)( name=([а-яА-Я ]+))?( numberOfChannels=([0-9]+))?$"),
+    CHANGE_PHONE("^set Phone id=([0-9]+)( name=([а-яА-Я ]+))?( callsMinCount=([0-9]+))?( smsCount=([0-9]+))?$"),
 
     SET_USER_TARIFF("^set User id=([0-9]+) service=(Internet|Television|Phone) idNewService=([0-9]+)$"),
     CHANGE_USER_INTERNET("^set User id=([0-9]+) service=Internet( speed=([0-9]+))?( antivirus=(true|false))?( connectionType=(ADSL|DialUp|ISDN|Cable|Fiber))?$"),
@@ -25,15 +25,15 @@ public enum Command {
     CREATE_PHONE("^create Phone name=([а-яА-Я0-9 ]+) callsMinCount=([0-9]+) smsCount=([0-9]+)$"),
     CREATE_TELEVISION("^create name=([а-яА-Я0-9 ]+) Television numberOfChannels=([0-9]+)$"),
 
-    /**/DELETE_USER("^delete User id=([0-9]+)$"),
-    /**/DELETE_TARIFF_FROM_USER("^delete User id=([0-9]+) service=(Internet|Television|Phone)$"),
-    /**/DELETE_TARIFF("^delete (Internet|Television|Phone) id=([0-9]+)$");
+    DELETE_USER("^delete User id=([0-9]+)$"),
+    DELETE_TARIFF_FROM_USER("^delete User id=([0-9]+) service=(Internet|Television|Phone)$"),
+    DELETE_TARIFF("^delete (Internet|Television|Phone) id=([0-9]+)$");
 
 
     private final String regex;
     private String rawCommand;
 
-    private Command(final String regex) {
+    Command(final String regex) {
         this.regex = regex;
     }
 
@@ -41,13 +41,13 @@ public enum Command {
         return this.regex;
     }
 
-    public String getRawCommand() {
-        return this.rawCommand;
-    }
+//    public String getRawCommand() {
+//        return this.rawCommand;
+//    }
 
-    public void setRawCommand(final String rawCommand) {
-        this.rawCommand = rawCommand;
-    }
+//    public void setRawCommand(final String rawCommand) {
+//        this.rawCommand = rawCommand;
+//    }
 
     public static Optional<Command> parseCommand(final String rawString) {
         Matcher matcher;

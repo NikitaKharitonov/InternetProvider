778324f706fe5972f1476e6fdf02b7a79bd17955
NikitaKharitonov <nv.kharitonov@yandex.ru>
2019-12-09 22:51:37 +0400

    Update Models
    Add static methods for reading/writing services and users from/to an XML file:
    - writeUsersToXML
    - readUsersFromXML
    - writeServicesToXML
    - readServicesFromXML

--- src/model/models/Models.java
+++ src/model/models/Models.java
@@ -3,11 +3,19 @@ package model.models;
 import model.services.*;
 import model.users.User;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.*;
+
+import javax.xml.parsers.*;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 public class Models {
     /*
@@ -89,6 +97,94 @@ public class Models {
             return services;
         } catch (IOException e) {
             throw new IOException("Failed to read services from file");
+        }
+    }
+
+    public static void writeUsersToXML(HashMap<Long, User> users, String filePath) throws IOException {
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            XMLOutputFactory xMLOutputFactory = XMLOutputFactory.newInstance();
+            XMLStreamWriter xMLStreamWriter = xMLOutputFactory.createXMLStreamWriter(writer);
+            xMLStreamWriter.writeStartDocument();
+            xMLStreamWriter.writeCharacters("\n");
+            xMLStreamWriter.writeStartElement("users");
+            xMLStreamWriter.writeCharacters("\n");
+            for (Long id: users.keySet())
+                users.get(id).toXML(xMLStreamWriter);
+            xMLStreamWriter.writeEndElement();
+            xMLStreamWriter.writeCharacters("\n");
+            xMLStreamWriter.writeEndDocument();
+            xMLStreamWriter.flush();
+            xMLStreamWriter.close();
+        } catch (XMLStreamException | IOException e) {
+            throw new IOException("Failed to write users to XML file");
+        }
+    }
+
+    public static HashMap<Long, User> readUsersFromXML(String filePath) throws IOException {
+        try {
+            File inputFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("user");
+            HashMap<Long, User> users = new HashMap<>();
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    User user = new User(eElement);
+                    users.put(user.getId(), user);
+                }
+            }
+            return users;
+        } catch (ParserConfigurationException | SAXException e) {
+            throw new IOException("Failed to read users from XML file");
+        }
+    }
+
+    public static void writeServicesToXML(HashMap<Long, Service> services, String filePath) throws IOException {
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            XMLOutputFactory xMLOutputFactory = XMLOutputFactory.newInstance();
+            XMLStreamWriter xMLStreamWriter = xMLOutputFactory.createXMLStreamWriter(writer);
+            xMLStreamWriter.writeStartDocument();
+            xMLStreamWriter.writeCharacters("\n");
+            xMLStreamWriter.writeStartElement("services");
+            xMLStreamWriter.writeCharacters("\n");
+            for (Long id: services.keySet())
+                services.get(id).toXML(xMLStreamWriter);
+            xMLStreamWriter.writeEndElement();
+            xMLStreamWriter.writeCharacters("\n");
+            xMLStreamWriter.writeEndDocument();
+            xMLStreamWriter.flush();
+            xMLStreamWriter.close();
+        } catch (XMLStreamException | IOException e) {
+            throw new IOException("Failed to write services to XML file");
+        }
+    }
+
+    public static HashMap<Long, Service> readServicesFromXML(String filePath) throws IOException {
+        try {
+            File inputFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("service");
+            HashMap<Long, Service> users = new HashMap<>();
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    Service service = ServiceFactory.createService(eElement);
+                    users.put(service.getId(), service);
+                }
+            }
+            return users;
+        } catch (ParserConfigurationException | SAXException e) {
+            throw new IOException("Failed to read services from XML file");
         }
     }
 }

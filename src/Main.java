5371a021d0b27d3d29297b4bfa88aabc30b993a2
NikitaKharitonov <nv.kharitonov@yandex.ru>
2019-12-11 18:52:40 +0400

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-import controller.BaseController;
-import controller.FailedOperation;
-import model.models.FileModel;
-import view.ConsoleView;
-import view.View;
+import server.controller.BaseController;
+import server.controller.FailedOperation;
+import server.model.data_storage_factories.XMLFileDataStorageFactory;
+import server.model.models.Model;
+import server.view.ConsoleView;
+import server.view.View;
+import server.model.models.BaseModel;
 
 import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Model model = new BaseModel();
+            model.setDataStorageFactory(new XMLFileDataStorageFactory());
+            model.read();
+            View view = new ConsoleView(new BaseController(model));
+            view.run();
+        } catch (IOException | FailedOperation e) {
+            System.err.println(e.getMessage());
+        }
 
-    public static void main(String[] args) throws IOException, FailedOperation {
-        View view = new ConsoleView(new BaseController(new FileModel()));
-        view.run();
     }
 }

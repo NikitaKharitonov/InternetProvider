d6ee6e15a486bb9b14da4ed322ae7bd5a7eea5e0
NikitaKharitonov <nv.kharitonov@yandex.ru>
2020-02-15 22:01:14 +0400

    Add serviceToString method in Services

--- src/model/services/Services.java
+++ src/model/services/Services.java
@@ -4,6 +4,12 @@ import org.w3c.dom.Element;
 
 public class Services {
 
+    public static String[] serviceTypes = {
+            Internet.class.getSimpleName(),
+            Phone.class.getSimpleName(),
+            Television.class.getSimpleName()
+    };
+
     public static Service createService(Element element) {
         String type = element.getElementsByTagName("type").item(0).getTextContent();
         if (type.equals(Internet.class.getSimpleName()))
@@ -15,7 +21,7 @@ public class Services {
         return null;
     }
 
-    public static Internet createInternet (Element element) {
+    private static Internet createInternet (Element element) {
         long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
         String name = element.getElementsByTagName("name").item(0).getTextContent();
         int speed = Integer.parseInt(element.getElementsByTagName("speed").item(0).getTextContent());
@@ -24,7 +30,7 @@ public class Services {
         return new Internet(id, name, speed, antivirus, connectionType);
     }
 
-    public static Phone createPhone(Element element) {
+    private static Phone createPhone(Element element) {
         long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
         String name = element.getElementsByTagName("name").item(0).getTextContent();
         int callsMinCount = Integer.parseInt(element.getElementsByTagName("number_of_calling_minutes").item(0).getTextContent());
@@ -32,10 +38,39 @@ public class Services {
         return new Phone(id, name, callsMinCount, smsCount);
     }
 
-    public static Television createTelevision(Element element) {
+    private static Television createTelevision(Element element) {
         long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
         String name = element.getElementsByTagName("name").item(0).getTextContent();
         int numberOfChannels = Integer.parseInt(element.getElementsByTagName("number_of_channels").item(0).getTextContent());
         return new Television(id ,name, numberOfChannels);
+    }
+
+    public static String serviceToString(Service service) {
+        String type = service.getType();
+        if (type.equals(Internet.class.getSimpleName()))
+            return internetToString((Internet)service);
+        else if (type.equals(Phone.class.getSimpleName()))
+            return writePhone((Phone)service);
+        else if (type.equals(Television.class.getSimpleName()))
+            return writeTelevision((Television)service);
+        return null;
+    }
+
+    private static String internetToString(Internet internet) {
+        return "Name: " + internet.getName() + "\n" +
+                "Speed: " + internet.getSpeed() + "\n" +
+                "Connection type: " + internet.getConnectionType() + "\n" +
+                "Antivirus: " + internet.isAntivirus();
+    }
+
+    private static String writePhone(Phone phone) {
+        return "Name: " + phone.getName() + "\n" +
+                "Number of minutes: " + phone.getCallsMinCount() + "\n" +
+                "Number of SMS: " + phone.getSmsCount();
+    }
+
+    private static String writeTelevision(Television television) {
+        return "Name: " + television.getName() + "\n" +
+                "Number of channels: " + television.getNumberOfChannels();
     }
 }

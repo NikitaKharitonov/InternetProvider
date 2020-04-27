24bcae65f3b5ea57ecf80431c75d3b270497ada7
NikitaKharitonov <nv.kharitonov@yandex.ru>
2020-04-27 11:33:50 +0400

    Add the DeleteClient servlet

---
+++ src/main/java/ru/internetprovider/servlets/DeleteClient.java
@@ -0,0 +1,26 @@
+package ru.internetprovider.servlets;
+
+import ru.internetprovider.model.DBModel;
+import ru.internetprovider.model.exceptions.ClientNotFoundException;
+import ru.internetprovider.model.exceptions.InvalidModelException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DeleteClient", urlPatterns = "/deleteClient")
+public class DeleteClient extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long clientId = Long.parseLong(request.getParameter("clientId"));
+        DBModel dbModel = new DBModel();
+        try {
+            dbModel.removeClient(clientId);
+        } catch (ClientNotFoundException | InvalidModelException e) {
+            throw new ServletException(e.getMessage());
+        }
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+}
